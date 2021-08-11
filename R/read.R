a615863d6047fe2d35945ee9c5defcbbedb2c095
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-08-11 15:16:54 -0400

    Combine ref, alt, and coverage tables
    These three tables will always be generated by MIPTools and it is easiest to read them together.

--- R/read.R
+++ R/read.R
@@ -1,19 +1,22 @@
 #------------------------------------------------
 #' Read data
 #'
-#' Read paired data files and combine the files.
+#' Read files containing MIPtools' data tables and combine them.
 #'
-#' @param alternate_table Alternate table.
-#' @param coverage_table Coverage table.
+#' @param reference_table File path to the reference table.
+#' @param alternate_table File path to the alternate table.
+#' @param coverage_table File path to the coverage table.
 #'
 #' @return A tibble containing the parsed data.
 #' @export
-read <- function(alternate_table, coverage_table) {
-  alternate_table <- read_table(alternate_table, "alternate")
-  coverage_table  <- read_table(coverage_table, "coverage")
+read <- function(reference_table, alternate_table, coverage_table) {
+  reference_table <- read_file(reference_table, "reference")
+  alternate_table <- read_file(alternate_table, "alternate")
+  coverage_table  <- read_file(coverage_table, "coverage")
 
-  bind_table <- dplyr::full_join(alternate_table, coverage_table) %>%
-    dplyr::mutate(dplyr::across(.data$umi_count:.data$coverage, as.numeric))
+  bind_table <- dplyr::full_join(reference_table, alternate_table) %>%
+    dplyr::full_join(coverage_table) %>%
+    dplyr::mutate(dplyr::across(.data$ref_umi_count:.data$coverage, as.numeric))
 }
 
 #------------------------------------------------
@@ -25,9 +28,8 @@ read <- function(alternate_table, coverage_table) {
 #' @param type Whether the table is an alternate or coverage table.
 #'
 #' @return Parsed file.
-#'
-read_table <- function(file, type = c("alternate", "coverage")) {
 #' @keywords internal
+read_file <- function(file, type = c("reference", "alternate", "coverage")) {
   data <- vroom::vroom(file, col_names = FALSE, show_col_types = FALSE) %>%
     # Take the transpose of our matrix, making rows columns and columns rows.
     # This will allows us to keep all the data in our .csv file.
@@ -38,13 +40,20 @@ read_table <- function(file, type = c("alternate", "coverage")) {
     # Assign the column names of our tibble and clean them up
     dplyr::select(-.data$name) %>%
     janitor::row_to_names(1)
-  if (type == "alternate") {
+  if (type == "reference") {
     data <- data %>%
       # Convert out data to a long format, where now we have a column with the
-      # umi_count
+      # ref_umi_count
       tidyr::pivot_longer(cols = -c(1:6),
                           names_to = "sample" ,
-                          values_to = "umi_count")
+                          values_to = "ref_umi_count")
+  } else if (type == "alternate") {
+    data <- data %>%
+      # Convert out data to a long format, where now we have a column with the
+      # alt_umi_count
+      tidyr::pivot_longer(cols = -c(1:6),
+                          names_to = "sample" ,
+                          values_to = "alt_umi_count")
   } else if (type == "coverage") {
     data <- data %>%
       # Convert out data to a long format, where now we have a column with the
