7142857cb4772c0023be08f4e721ce9235ea65c3
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-09-08 17:55:06 -0400

    Implement natural sorting (#6)
    * Implement natural sorting
    
    * Add sorting tests
    
    * Preserve ordering of sorted columns
    
    * Arrange additional column types

---
+++ R/arrange_natural.R
@@ -0,0 +1,71 @@
+#------------------------------------------------
+#' Arrange rows by column values naturally
+#'
+#' @description
+#' `arrange_natural()` orders the rows of a data frame by the values of selected
+#' columns.
+#'
+#' Data is naturally sorted (see *Details*, below) in ascending order. Columns
+#' used for sorting are converted to factors to preserve their ordering.
+#' Grouping is ignored.
+#'
+#' @details
+#' ## Natural sorting
+#' `arrange_natural()` is built on [dplyr::arrange()] to provide
+#' \href{https://en.wikipedia.org/wiki/Natural_sort_order}{natural sorting}
+#' (sorting of strings with both letters and numerals). The underlying
+#' implementation for natural sorting is based on the
+#' \href{https://stringi.gagolewski.com/}{`stringi`} library.
+#'
+#' @param .data A data frame, data frame extension (e.g. a tibble), or a lazy
+#'   data frame (e.g. from dbplyr or dtplyr).
+#' @param ... <[`data-masking`][dplyr_data_masking]> Variables to sort by.
+#'
+#' @return
+#' An object of the same type as `.data`. The output has the following
+#' properties:
+#'
+#' * All rows appear in the output, but (usually) in a different place.
+#' * Sorted columns are converted to factors.
+#' * All other columns are not modified.
+#' * Data frame attributes are preserved.
+#'
+#' @seealso [dplyr::arrange()]
+#' @export
+#' @examples
+#' df <- tibble::tribble(
+#'   ~sample, ~gene,
+#'   "D10-23", "atp6",
+#'   "D10-43", "mdr1",
+#'   "D10-55", "atp6",
+#'   "D10-5", "mdr1",
+#'   "D10-47", "dhps",
+#'   "D10-15", "atp6"
+#' )
+#'
+#' arrange_natural(df, sample)
+#' df %>% arrange_natural(sample, gene)
+arrange_natural <- function(.data, ...) {
+  # Store variables to arrange by
+  dots <- enquos(..., .named = TRUE)
+
+  if (requireNamespace("stringi", quietly = TRUE) &
+      requireNamespace("purrr", quietly = TRUE)) {
+    # Manipulate dots to get arrange variables
+    arrange_vars <- purrr::map(dots, function(var) {
+      expr(stringi::stri_rank(as.character(!!var), numeric = TRUE))
+    })
+
+    # Manipulate dots to get mutate variables
+    mutate_vars <- purrr::map(dots, function(var) {
+      expr(forcats::as_factor(as.character(!!var)))
+    })
+  } else {
+    warning("Packages \"purrr\" and \"stringi\" needed for natural sorting. Please install them.")
+  }
+
+  # Call dplyr::arrange
+  .data %>%
+    dplyr::arrange(!!!arrange_vars) %>%
+    dplyr::mutate(!!!mutate_vars)
+}
---
+++ tests/testthat/test-arrange_natural.R
@@ -0,0 +1,76 @@
+df <- tibble::tribble(
+  ~sample, ~gene,
+  "D10-55", "atp6",
+  "D10-5", "mdr1",
+  "D10-5", "atp6",
+  "D10-15", "atp6",
+)
+
+test_that("empty arrange_natural() returns input", {
+  expect_identical(arrange_natural(df), df)
+})
+
+test_that("applies natural sorting", {
+  rf <- tibble::tribble(
+    ~sample, ~gene,
+    "D10-5", "mdr1",
+    "D10-5", "atp6",
+    "D10-15", "atp6",
+    "D10-55", "atp6",
+  ) %>%
+    dplyr::mutate(sample = forcats::as_factor(sample))
+
+  expect_identical(arrange_natural(df, sample), rf)
+})
+
+test_that("sorts on multiple variables", {
+  rf <- tibble::tribble(
+    ~sample, ~gene,
+    "D10-5", "atp6",
+    "D10-5", "mdr1",
+    "D10-15", "atp6",
+    "D10-55", "atp6",
+  ) %>%
+    dplyr::mutate(sample = forcats::as_factor(sample),
+                  gene = forcats::as_factor(gene))
+
+  expect_identical(arrange_natural(df, sample, gene), rf)
+})
+
+test_that("arranges factors", {
+  fdf <- df %>%
+    dplyr::mutate(dplyr::across(.fns = ~ forcats::as_factor(.)))
+
+  rf <- tibble::tribble(
+    ~sample, ~gene,
+    "D10-5", "mdr1",
+    "D10-5", "atp6",
+    "D10-15", "atp6",
+    "D10-55", "atp6",
+  ) %>%
+    dplyr::mutate(sample = forcats::as_factor(sample),
+                  gene = forcats::fct_relevel(forcats::as_factor(gene), "atp6"))
+
+  expect_identical(arrange_natural(fdf, sample), rf)
+})
+
+test_that("arranges numerics", {
+  ndf <- tibble::tribble(
+    ~sample, ~pos,
+    "D10-55", 55,
+    "D10-5", 10,
+    "D10-5", 5,
+    "D10-15", 15,
+  )
+
+  rf <- tibble::tribble(
+    ~sample, ~pos,
+    "D10-5", 5,
+    "D10-5", 10,
+    "D10-15", 15,
+    "D10-55", 55,
+  ) %>%
+    dplyr::mutate(pos = forcats::as_factor(pos))
+
+  expect_identical(arrange_natural(ndf, pos), rf)
+})
