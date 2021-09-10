3f1cc5bce64b689c2a2a0343fe6a5e31241d5597
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-09-10 16:51:51 -0400

    Add imports

--- R/utils-tidy-eval.R
+++ R/utils-tidy-eval.R
@@ -40,7 +40,7 @@
 #' @md
 #' @name tidyeval
 #' @keywords internal
-#' @importFrom rlang expr enquo enquos sym syms .data := as_name as_label
+#' @import rlang
 #' @aliases expr enquo enquos sym syms .data := as_name as_label
 #' @export expr enquo enquos sym syms .data := as_name as_label
 NULL
