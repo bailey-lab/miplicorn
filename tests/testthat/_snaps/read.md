1a1ee9766918b0c02c569e733963869035a821f6
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-09-14 12:31:21 -0400

    Test for errors and deprecation warnings

---
+++ tests/testthat/_snaps/read.md
@@ -0,0 +1,25 @@
+# chrom and gene are deprecated
+
+    Code
+      suppressMessages(read("small.csv", "small.csv", "small.csv", chrom = "13",
+        gene = "10"))
+    Warning <lifecycle_warning_deprecated>
+      The `chrom` argument of `read()` is deprecated as of MIPr 0.0.0.9001.
+      Please use the `...` argument instead to filter data.
+      The `gene` argument of `read()` is deprecated as of MIPr 0.0.0.9001.
+      Please use the `...` argument instead to filter data.
+    Error <rlang_error>
+      Multiple filtering criteria selected.
+      x Cannot filter on both `chrom` and `gene`.
+      i Select only one piece of information to filter on.
+
+# named filter inputs returns error
+
+    Code
+      read_file("small.csv", gene = "mdr1", .name = value)
+    Error <rlang_error>
+      Problem with `read()` input `..1`.
+      x Input `..1` is named
+      i This usually means that you've used `=` instead of `==`.
+      i Did you mean `gene == "mdr1"`?
+
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -9,3 +9,31 @@ test_that("read_file returns an empty tibble on an empty file", {
 test_that("read returns an empty tibble on an empty file", {
   expect_equal(read("empty-file", "empty-file", "empty-file"), tibble::tibble())
 })
+
+test_that("chrom and gene are deprecated", {
+  expect_warning(suppressMessages(
+    read("small.csv", "small.csv", "small.csv", chrom = "13"))
+  )
+  expect_warning(suppressMessages(
+    read("small.csv", "small.csv", "small.csv", gene = "atp"))
+  )
+
+  expect_error(suppressMessages(suppressWarnings(
+    read("small.csv", "small.csv", "small.csv", chrom = "13", gene = "atp")))
+  )
+  expect_snapshot(
+    error = TRUE,
+    suppressMessages(
+      read("small.csv", "small.csv", "small.csv", chrom = "13", gene = "10")
+    )
+  )
+})
+
+test_that("named filter inputs returns error", {
+  expect_error(read_file("small.csv", gene = "mdr1", .name = value))
+
+  expect_snapshot(
+    error = TRUE,
+    read_file("small.csv", gene = "mdr1", .name = value)
+  )
+})
