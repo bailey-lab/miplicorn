d79a103e5a97f7c1e4959497d80fe3c011618f71
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-09-17 13:47:57 -0400

    Test themes

---
+++ tests/testthat/test-theme_miplicorn.R
@@ -0,0 +1,8 @@
+test_that("themes are stable", {
+  df <- tibble::tibble(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
+  plot <- ggplot2::ggplot(df, ggplot2::aes(x, y, colour = z)) +
+    ggplot2::geom_point()
+
+  vdiffr::expect_doppelganger("default_theme", plot + default_theme())
+  vdiffr::expect_doppelganger("theme_miplicorn", plot + theme_miplicorn())
+})
