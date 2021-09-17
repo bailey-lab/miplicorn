6df3661ff916d6e4806f8a9f3350f808ebb3c7e2
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-09-17 12:51:53 -0400

    Create custom theme

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,36 @@ empty_file <- function(x) {
     file.exists(x) &&
     file.info(x, extra_cols = FALSE)$size == 0
 }
+
+#------------------------------------------------
+#' Custom ggplot2 theme
+#'
+#' Custom ggplot2 theme that builds off of `ggplot2::theme_classic()`.
+#'
+#' @inheritParams ggplot2::theme_classic
+#'
+#' @importFrom ggplot2 %+replace%
+#' @export
+#' @examples
+#' library("ggplot2")
+#' p <- ggplot(mtcars, aes(x = wt, y = mpg, colour = factor(gear))) +
+#'   geom_point() +
+#'   facet_wrap(~am) +
+#'   geom_smooth(method = "lm", se = FALSE) +
+#'   theme_miplicorn()
+#' p
+theme_miplicorn <- function(base_size = 10, base_family = "",
+                            base_line_size = base_size / 22,
+                            base_rect_size = base_size / 22) {
+  ggplot2::theme_classic(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) %+replace%
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(hjust = 0.5, size = 12),
+      legend.position = "right",
+      complete = TRUE
+    )
+}
