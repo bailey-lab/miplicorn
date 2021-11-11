b43fddb94caf07e266eb11f2ea1e71d3ead2b29f
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-11-11 14:44:02 -0500

    Separate theme tests

--- tests/testthat/test-themes.R
+++ tests/testthat/test-themes.R
@@ -1,10 +1,16 @@
-test_that("themes are stable", {
+test_that("theme_miplicorn is stable", {
   df <- tibble::tibble(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
   plot <- ggplot2::ggplot(df, ggplot2::aes(x, y, colour = z)) +
     ggplot2::geom_point()
 
   vdiffr::expect_doppelganger("default_theme", plot + default_theme())
   vdiffr::expect_doppelganger("theme_miplicorn", plot + theme_miplicorn())
+})
+
+test_that("theme_rainbow is stable", {
+  df <- tibble::tibble(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
+  plot <- ggplot2::ggplot(df, ggplot2::aes(x, y, colour = z)) +
+    ggplot2::geom_point()
 
   vdiffr::expect_doppelganger("rainbow_theme", plot + rainbow_theme())
   vdiffr::expect_doppelganger("theme_rainbow", plot + theme_rainbow())
