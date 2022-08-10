5eb9b9fcd5d432b9b73aeda8b5c44b8839ddbce8
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-08-10 12:46:04 +0300

    Improve utility functions
    * Use wrapper to get file size
    * Send output to null device

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,13 @@
+# Check if file is empty
 empty_file <- function(x) {
   is.character(x) &&
     file.exists(x) &&
-    file.info(x, extra_cols = FALSE)$size == 0
+    file.size(x) == 0
 }
 
 # Hide text output
 quiet <- function(x) {
-  sink(tempfile())
+  sink(nullfile())
   on.exit(sink())
   invisible(force(x))
 }
